987171706366eb864a45bb50e2a3daedad27bed3
k3rnll <kanaikin.greg@gmail.com>
2024-02-23 22:57:24 +0300

    clean up main file

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
-import map
-import entities
-import creatures
-import renders
 import time
-
+import map
+import renders
 import simulation
 
 
 def main():
-    map1 = map.Map(12, 24)
-    my_simulation = simulation.Simulation(map1, 30)
-#    print(f"x: {predator1.position.x}, y: {predator1.position.y}")
-#    predator1.make_move("down")
-#    print(f"x: {predator1.position.x}, y: {predator1.position.y}")
-#    print(map1.entities.items())
+    map1 = map.Map(32, 64)
+    my_simulation = simulation.Simulation(map1, 250)
     renders.render_map(map1)
     while 1:
         my_simulation.spin_the_world()
         renders.render_map(map1)
-        time.sleep(0.1)
+        time.sleep(0.01)
 
 
 if __name__ == "__main__":
