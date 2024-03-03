592bfc1ea6719ac5d0aa46f1a82896149c8acf68
k3rnll <kanaikin.greg@gmail.com>
2024-03-03 09:20:48 +0300

    cleanup: view class

--- view.py
+++ view.py
@@ -7,16 +7,16 @@ def _get_clear_display_str():
 
 
 class View:
-    def __init__(self, data_model: model.Model, controller, is_fps=True):
+    def __init__(self, data_model: model.Model, controller, is_show_fps=True):
         self.__model = data_model
         self._controller = controller
-        self.__is_fps = is_fps
-        self.__empty_cell_icon = '_'
+        self.__is_show_fps = is_show_fps
+        self.__empty_cell_icon = ' '
         self.__last_call_time = 0
 
-    def __fps_info(self) -> str:
+    def __fps_info(self):
         cur_time = time.time_ns()
-        fps_str = f"fps:\t{str(1000000000 // (cur_time - self.__last_call_time))}"
+        fps_str = f"fps:\t{str(1000000000 // (cur_time - self.__last_call_time))}\n"
         self.__last_call_time = cur_time
         return fps_str
 
@@ -34,7 +34,7 @@ class View:
         return frame
 
     def __make_frame(self):
-        if self.__is_fps:
+        if self.__is_show_fps:
             return self.__grid_render() + self.__fps_info()
         return self.__grid_render()
 
