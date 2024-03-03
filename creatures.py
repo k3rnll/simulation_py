a974f33418a130349bd5bbc6bc3dc7e6841b3d47
k3rnll <kanaikin.greg@gmail.com>
2024-03-03 20:38:19 +0300

    REF: split Creature class to MovementHandle

--- creatures.py
+++ creatures.py
@@ -1,50 +1,55 @@
 from abc import ABCMeta, abstractmethod
 import model
-import entities
+from entities import Entity, Position, Grass
 import random
 import coord
 
 
 class IMovable(metaclass=ABCMeta):
     @abstractmethod
-    def _move_up(self):
+    def move_up(self):
         pass
 
     @abstractmethod
-    def _move_down(self):
+    def move_down(self):
         pass
 
     @abstractmethod
-    def _move_left(self):
+    def move_left(self):
         pass
 
     @abstractmethod
-    def _move_right(self):
+    def move_right(self):
         pass
 
     @abstractmethod
-    def _move_up_left(self):
+    def move_up_left(self):
         pass
 
     @abstractmethod
-    def _move_up_right(self):
+    def move_up_right(self):
         pass
 
     @abstractmethod
-    def _move_down_left(self):
+    def move_down_left(self):
         pass
 
     @abstractmethod
-    def _move_down_right(self):
+    def move_down_right(self):
         pass
 
 
-class Creature(entities.Entity, IMovable):
+class Creature(Entity):
     def __init__(self, icon: str):
         super().__init__(icon)
         self._hp = 100
         self.__model = None
-        self.vision = Vision(self)
+        self.__vision = Vision(self)
+        self.__movement = MovementHandle(self)
+
+    @abstractmethod
+    def eat(self):
+        pass
 
     @property
     def model(self) -> model.Model | None:
@@ -53,103 +58,86 @@ class Creature(entities.Entity, IMovable):
     def set_model(self, to_model):
         self.__model = to_model
 
-    @classmethod
-    def is_creature(cls, obj) -> bool:
-        return isinstance(obj, Creature)
-
     @property
     def icon(self) -> str:
-        if self._hp <= 0:
-            return 'X'
-        return super().icon
+        return super().icon if self._hp > 0 else 'X'
 
     @property
     def is_dead(self) -> bool:
         return self._hp <= 0
 
-    def _move_up(self):
-        if self.model:
-            new_point = entities.Position(self.position.x, self.position.y - 1)
-            self.model.change_entity_position(self, new_point)
-
-    def _move_down(self):
-        if self.model:
-            new_point = entities.Position(self.position.x, self.position.y + 1)
-            self.model.change_entity_position(self, new_point)
-
-    def _move_left(self):
-        if self.model:
-            new_point = entities.Position(self.position.x - 1, self.position.y)
-            self.model.change_entity_position(self, new_point)
-
-    def _move_right(self):
-        if self.model:
-            new_point = entities.Position(self.position.x + 1, self.position.y)
-            self.model.change_entity_position(self, new_point)
-
-    def _move_up_right(self):
-        self._move_up()
-        self._move_right()
-
-    def _move_up_left(self):
-        self._move_up()
-        self._move_left()
-
-    def _move_down_right(self):
-        self._move_down()
-        self._move_right()
-
-    def _move_down_left(self):
-        self._move_down()
-        self._move_left()
-
-    def make_move(self, direction):
-        # left top corner is x=0, y=0
-        if direction == "up":
-            self._move_up()
-        elif direction == "down":
-            self._move_down()
-        elif direction == "left":
-            self._move_left()
-        elif direction == "right":
-            self._move_right()
-        elif direction == "up_right" or direction == "right_up":
-            self._move_up_right()
-        elif direction == "up_left" or direction == "up_left":
-            self._move_up_left()
-        elif direction == "down_right" or direction == "right_down":
-            self._move_down_right()
-        elif direction == "down_left" or direction == "left_down":
-            self._move_down_left()
-
-    def make_random_move(self):
-        commands = ["up", "down", "left", "right", "up_right", "up_left", "down_right", "down_left"]
-        x = random.randrange(0, len(commands))
-        self.make_move(commands[x])
-
     @property
     def entities_in_sight(self) -> set:
-        return self.vision.entities_in_sight
+        return self.__vision.entities_in_sight
 
-    def get_next_point_to_target(self, target: entities.Entity) -> entities.Position:
-        return coord.get_points_of_vector(self.position, target.position)[0]
+    def make_random_move(self):
+        x = random.randrange(0, 8)
+        match x:
+            case 0: self.__movement.move_up()
+            case 1: self.__movement.move_down()
+            case 2: self.__movement.move_left()
+            case 3: self.__movement.move_right()
+            case 4: self.__movement.move_up_left()
+            case 5: self.__movement.move_up_right()
+            case 6: self.__movement.move_down_left()
+            case 7: self.__movement.move_down_right()
 
-    def change_hp(self, amount: int):
+    def change_hp_by(self, amount: int):
         if 0 < self._hp <= 100:
             self._hp += amount
             self._hp = 100 if self._hp > 100 else self._hp
             self._hp = 0 if self._hp < 0 else self._hp
 
-    @abstractmethod
-    def eat(self):
-        pass
+    def _get_next_point_to_target(self, target: Entity) -> Position:
+        return coord.get_points_of_vector(self.position, target.position)[0]
+
+
+class MovementHandle(IMovable):
+    def __init__(self, owner: Creature):
+        self.__owner = owner
+
+    def move_up(self):
+        if self.__owner.model:
+            new_point = Position(self.__owner.position.x, self.__owner.position.y - 1)
+            self.__owner.model.change_entity_position(self.__owner, new_point)
+
+    def move_down(self):
+        if self.__owner.model:
+            new_point = Position(self.__owner.position.x, self.__owner.position.y + 1)
+            self.__owner.model.change_entity_position(self.__owner, new_point)
+
+    def move_left(self):
+        if self.__owner.model:
+            new_point = Position(self.__owner.position.x - 1, self.__owner.position.y)
+            self.__owner.model.change_entity_position(self.__owner, new_point)
+
+    def move_right(self):
+        if self.__owner.model:
+            new_point = Position(self.__owner.position.x + 1, self.__owner.position.y)
+            self.__owner.model.change_entity_position(self.__owner, new_point)
+
+    def move_up_right(self):
+        self.move_up()
+        self.move_right()
+
+    def move_up_left(self):
+        self.move_up()
+        self.move_left()
+
+    def move_down_right(self):
+        self.move_down()
+        self.move_right()
+
+    def move_down_left(self):
+        self.move_down()
+        self.move_left()
 
 
 class Vision:
-    def __init__(self, owner: Creature, distance=5):
+    def __init__(self, owner: Creature, distance=10):
         self.__owner = owner
         self.__distance = distance
-        self.__solid_obj_types = entities.Entity
+        self.__solid_obj_types = Entity
         self.__entities_in_sight = set()
 
     @property
@@ -191,7 +179,7 @@ class Predator(Creature):
             if cell:
                 for obj in cell.items:
                     if isinstance(obj, Herbivore) and not obj.is_dead:
-                        self.change_hp(self.__eat_hp)
+                        self.change_hp_by(self.__eat_hp)
                         obj.hit_by_bite(self.__eat_hp)
                         break
 
@@ -213,11 +201,11 @@ class Predator(Creature):
         if not target:
             self.make_random_move()
             return
-        next_point = self.get_next_point_to_target(target)
+        next_point = self._get_next_point_to_target(target)
         self.model.change_entity_position(self, next_point)
 
     def hit_by_hunger(self):
-        self.change_hp(-self.__hunger_hp)
+        self.change_hp_by(self.__hunger_hp * -1)
 
 
 class Herbivore(Creature):
@@ -229,16 +217,16 @@ class Herbivore(Creature):
     def eat(self):
         cell = self.model.grid.get_cell_on(self.position.x, self.position.y)
         for obj in cell.items:
-            if isinstance(obj, entities.Grass):
+            if isinstance(obj, Grass):
                 cell.remove_item(obj)
-                self.change_hp(self.__grass_hp)
+                self.change_hp_by(self.__grass_hp)
                 break
 
     def __choose_nearest_target(self, entities_set: set):
         distance_to_target = self.model.grid.height + self.model.grid.width
         chosen_entity = None
         for entity in entities_set:
-            if isinstance(entity, entities.Grass):
+            if isinstance(entity, Grass):
                 cur_dist = coord.calc_distance_to_point(self.position, entity.position)
                 if cur_dist < distance_to_target:
                     distance_to_target = cur_dist
@@ -252,11 +240,11 @@ class Herbivore(Creature):
         if not target:
             self.make_random_move()
             return
-        next_point = self.get_next_point_to_target(target)
+        next_point = self._get_next_point_to_target(target)
         self.model.change_entity_position(self, next_point)
 
     def hit_by_hunger(self):
-        self.change_hp(-self.__hunger_hp)
+        self.change_hp_by(self.__hunger_hp * -1)
 
     def hit_by_bite(self, amount: int):
-        self.change_hp(-amount)
+        self.change_hp_by(amount * -1)
